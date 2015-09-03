e059e6854948b18e4b9548b296097590e7b7f750
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-09-03 09:40:21 -0600

    initial commit

---
+++ user_input.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+
+puts "What is your firstname?"
+first_name = gets.chomp
+
+puts "What is your middlename?"
+middle_name = gets.chomp
+
+puts "What is your lastname?"
+last_name = gets.chomp
+
+puts "Hello #{first_name} #{middle_name} #{last_name} !"
