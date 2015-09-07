d979f0f45c56f35bdcc38cb8dacda4ec52f49f47
Kim Schlesinger <schlesinger.kim@gmail.com>
2015-09-07 13:31:57 -0600

    temp converter working

--- methods/temp_converter.rb
+++ methods/temp_converter.rb
@@ -1,32 +1,29 @@
 #!/usr/bin/ruby
 
-=begin
-Create a simple temperature convertor.
-Type 1 to convert from C to F
-Type 2 to convert from F to C
-=end
-
 # fahrenheit to C
 def C_to_F(c_num)
   (((9*Float(c_num))/5) + 32)
 end
-# puts C_to_F(3)
 
 # celsius to fahrenheit
-# def F_to_C(f_num)
-#   (f_num - 32) * 0.5555556
-# end
-# puts F_to_C(3)
+def F_to_C(f_num)
+  Float((f_num - 32) * 0.5555556)
+end
 
 # ask about conversion type
-print "Type '1' to convert from Celsius to Fahrenheit
+puts "Type '1' to convert from Celsius to Fahrenheit
 OR Type '2' to convert from Fahrenheit to Celsius \n"
 
 # conversion type
 conversion_num = gets.chomp
 if conversion_num == "1"
-  print "Enter Celsius Temperature:"
-  c_num = gets
+  puts "Enter Celsius Temperature:"
+  c_num = gets.chomp
   temp_in_F = C_to_F(c_num)
-  pring "#{c_num} degrees Celsius is #{temp_in_F} degrees Fahrenheit"
+  puts "#{c_num} degrees Celsius is #{temp_in_F} degrees Fahrenheit"
+elsif conversion_num =="2"
+  puts "Enter Fahrenheit Temperature:"
+  f_num = gets.chomp
+  temp_in_C = C_to_F(f_num)
+  puts "#{f_num} degrees Celsius is #{temp_in_C} degrees Fahrenheit"
 end
